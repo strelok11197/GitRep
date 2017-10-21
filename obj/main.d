23d65ae5f268b8b8892872caa40a098ebfc2c276
strelok11197 <bulenight@mail.ru>
2017-10-21 21:38:58 +0300

    First Commit

---
+++ obj/main.d
@@ -0,0 +1,7 @@
+.\obj\main.o: src\main.c
+.\obj\main.o: src\CMSIS/stm32f10x.h
+.\obj\main.o: src\CMSIS/core_cm3.h
+.\obj\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\obj\main.o: .\src\CMSIS\core_cmInstr.h
+.\obj\main.o: .\src\CMSIS\core_cmFunc.h
+.\obj\main.o: src\CMSIS/system_stm32f10x.h
