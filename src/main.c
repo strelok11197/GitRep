7e9bc0a43df3614c014ac4a8a62f65704130fcb8
strelok11197 <bulenight@mail.ru>
2017-12-03 01:30:05 +0300

    '4lab'

--- src/main.c
+++ src/main.c
@@ -1,90 +1,60 @@
 #include "CMSIS/stm32f10x.h"
 #include <stdint.h>
 
-int32_t strToInt(const char *str)
+void sort(int32_t * buf, uint32_t size) //Сортировка перемешиванием
 {
-	int64_t result = 0;
-	int i = 0;
-	_Bool flagMinus = 0;
-	_Bool flagPlus = 0;
+	uint32_t i, bot=0, top=size-1;
+	int32_t c;
 	
-	while(str[i] != '\0')
-	{		
-		if (str[i] == '-' && !flagPlus)				// Если обнаружен знак '-' и до этого не обнаруживалось цифр, то число отрицательное
-			flagMinus = 1;
+	while(bot<top)
+	{
 		
-		if (str[i] >=48 && str[i] <=57)
+		for (i=bot; i<top; i++) //проход вверх
 		{
-			flagPlus = 1;															// Если обнаружена цифра, обновить флаг
-			result = result * 10 + str[i] - 48;
+			if (buf[i] > buf[i+1])
+			{
+				c=buf[i];
+				buf[i]=buf[i+1];
+				buf[i+1]=c;
+			}
 		}
+		top--;
 		
-		i++;
+		for(i=top; i>bot; i--) //проход вниз
+		{
+			if (buf[i] < buf[i-1])
+			{
+				c=buf[i];
+				buf[i]=buf[i-1];
+				buf[i-1]=c;
+			}
+		}
+		bot++;
 	}
-		
-	if (flagMinus)
-		result = -result;
-	
-	return result;
 }
 
-uint8_t countSetBits(uint32_t n)
+
+int32_t getMedian(int32_t * buf, uint32_t size)
 {
-	uint8_t result = 0;
+	int32_t res[size], c;
+	uint32_t i=0;
 	
-	uint32_t nOld;
-	uint32_t mask = 1; 					// Маска с единицей в одном(правом) разряде
+	for (i=0; i<size; i++)
+	{ res[i]=buf[i]; }
 	
-	while	(mask<=0x80000000)
-	{
-		nOld = n;
-		n |= mask;
-		
-		if (nOld == n) result++; 			// Если при применении маски не изменилось число, значит в разряде, в котором у маски стоит 1, у числа тоже стоит 1
-		
-		if (mask < 0x80000000)
-			mask <<= 1; 						// Сдвиг единицы на 1 влево, если 1 не в самом левом разряде
-		else
-			break;
-	}
+	sort(res, size);
 	
-	return result;
+	return res[size/2];
 }
 
-uint8_t countLeadingZeros( uint32_t n)
-{
-	uint8_t result = 0;
-	
-	uint32_t nOld;
-	uint32_t mask = 0x80000000; // Маска с единицей в одном(левом) разряде
-	
-	while	(mask >= 1)
-	{
-		nOld = n;
-		n |= mask;
-		
-		if (nOld != n)
-			result++;										// Если при применении маски не изменилось число, значит в разряде, в котором у маски стоит 1, у числа тоже стоит 1
-		else
-			return result;
-
-		mask >>= 1; 							// Сдвиг единицы на 1 вправо
-	}
-	
-	return 32;											// Если не нули не найдены, значит это число 0, оно целиком состоит из 0
-}
 
 int main(void)
 {
-	int32_t numberInt;
-	const char numberStr[12] = "3d-s89ds5a"; // 12, т.к. самое длинное число -2147483648 (+ символ '\0')
-	numberInt = strToInt (numberStr);
+	int32_t Sort[10]={20,25,15,23,15,12,2,3,11,5};
+	sort(Sort, 10);
 	
-	uint8_t numberOnes;
-	numberOnes = countSetBits (15);
-	
-	uint8_t numberZeros;
-	numberZeros = countLeadingZeros(5);
+	int32_t Median[13]={20,25,15,18,50,23,15,12,2,48,3,11,5};
+	int32_t median = getMedian(Median, 13);
 	
 	while(1)
 	return 0;
